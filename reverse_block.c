2b209d91fb9be40001dc211af67e463df3bf2543
NagarajV9 <39578668+NagarajV9@users.noreply.github.com>
2018-07-27 07:55:51 +0530

    Create reverse_block.c

---
+++ reverse_block.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+struct employee
+{
+    int sal;
+    struct employee *next;
+}*head;
+
+void insert(int s)
+{
+  struct employee *temp,*temp1=head;
+  temp=(struct employee*)malloc(sizeof(struct employee));
+  temp->sal=s;
+  temp->next=NULL;
+  if(head==NULL)
+  head=temp;
+  else
+  {
+      while(temp1->next!=NULL)
+      temp1=temp1->next;
+      temp1->next=temp;
+  }
+}
+
+void display()
+{
+    struct employee *temp=head;
+    while(temp!=NULL)
+    {printf("%d\n",temp->sal);
+    temp=temp->next;
+    }
+}
+
+void sortl()
+{
+    int t1;
+    
+    struct employee *temp,*temp1;
+    for(temp=head;temp!=NULL;temp=temp->next)
+    {
+        
+      
+        for(temp1=temp;temp1!=NULL;temp1=temp1->next)
+        {
+            if(temp->sal>temp1->sal)
+            {
+               t1=temp1->sal;     
+               temp1->sal=temp->sal;
+               temp->sal=t1;
+            }
+        }
+    }
+}
+
+void reversel()
+{
+    struct employee *prev=NULL,*curr,*ne=NULL;
+    curr=head;
+    
+    while(curr!=NULL)
+    {
+        
+        ne=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=ne;
+    }
+    head=prev;
+}
+
+struct node *reverse(struct node *head,int k)
+{
+   struct node *prev=NULL,*next=NULL,*curr=head;
+   int count=0;
+}
+
+int main()
+{
+    int num,sal;
+    printf("\nEnter the no. of nodes ");
+    scanf("%d",&num);
+    printf("\nEnter the salary ");
+    while(num--)
+    {
+        scanf("%d",&sal);
+        insert(sal);
+    }
+    printf("\nBefore sorting\n");
+    display();
+    
+    printf("\nAfter sorting\n");
+    sortl();
+    display();
+    printf("\nAfter reversing\n");
+    reversel();
+    display();
+    reverse_block(head,3);
+    return 0;
+}
